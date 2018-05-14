9df7f0f72af50608d178e474e3e37eec05ee8718
proin <proin@proinlab.com>
2018-05-14 09:04:04 +0900

    first commit

---
+++ docs/attributeaggregator.txt
@@ -0,0 +1,154 @@
+Attribute Aggregator module
+==============
+
+The Attribute Aggregator module collects attributes from a SAML2 Attribute Authority
+by using SAML Attribute Query. It is implemented as an Authentication Processing Filter
+for [SimpleSAMLphp](https://simplesamlphp.org).
+
+The configuration of the module is in the `authproc.sp` section in `config/config.php`.
+You should configure the filter to run after the federated id, usually eduPersonPrincipalName
+is resolved.
+
+  * [Read more about processing filters in simpleSAMLphp](simplesamlphp-authproc)
+
+Install
+-------------------------------
+
+You can install the module with composer:
+
+    git clone https://github.com/kafe-federation/attributeaggregator
+
+The module depends on the `php-soap` extension.
+
+How to setup the attributeaggregator module
+-------------------------------
+
+The only required option of the module is the `entityId` of the Attribute Authority to 
+be queried. The AA must support the `urn:oasis:names:tc:SAML:2.0:bindings:SOAP` binding.
+
+Note that when you refer to attribute names, you must use the OID's of the attributes.
+
+Example:
+
+                59 => array(
+                   'class' => 'attributeaggregator:attributeaggregator',
+                   'entityId' => 'https://aa.example.com:8443/aa',
+
+                  /**
+                   * The subject of the attribute query. Default: urn:oid:1.3.6.1.4.1.5923.1.1.1.6 (eduPersonPrincipalName)
+                   */
+                   //'attributeId' => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.6',
+
+                   /** 
+                    * If set to TRUE, the module will throw an exception if the attribute query can not be
+                    * attempted or when there is an error during the query. If FALSE, the request processing
+                    * will resume on errors.
+                    */
+                   // 'required' => FALSE,
+
+                   /** 
+                    * The format of attributeId. Default is 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent'
+                    */
+                   //'nameIdFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+
+
+                   /**
+                    * The name Format of the attribute names.
+                    */
+                   //'attributeNameFormat' => 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
+
+                   /**
+                    * The 'attributes' element specifies what to do with the attributes received from the AA.
+                    * The keys of the array is the attribute name in (''urn:oid'') format.
+                    * A special index '*' applies to all attributes that are not specified as an array index.
+                    * If the element is undefined or empty, the filter will request all attributes and all
+                    * received attributes will be merged.
+                    *
+                    * For each attributes the following filters can be specified:
+                    *
+                    *   values:
+                    *     the array of acceptable values. If not defined, the filter will accept all values.
+                    *   multiSource:
+                    *     merge:    merge the existing and the new values, this is the default behaviour,
+                    *     override: drop the existing values and set the values from AA,
+                    *     keep:     drop the new values from AA and preserve the original values. Note that it
+                    *               only preserves existing attributes, so if an attribute does not exist
+                    *               before the filter is run, the values provided by the AA will be assigned.
+                    *               Thus, if you want to limit what attributes you accept from the AA, you
+                    *               can not use the default ('*') rule.
+                    */
+                   // 'attributes' => array(
+                   //         // This rule overrides urn:oid:attribute-OID-1 if the obtained values are
+                   //         // one of "value1" or "value2".
+                   //         "urn:oid:attribute-OID-1" => array (
+                   //               "values" => array ("value1", "value2"),
+                   //               "multiSource" => "override"
+                   //               ),
+                   //         // This rule will add urn:oid:attribute-OID-2 only if it has not been set
+                   //         // previously.
+                   //         "urn:oid:attribute-OID-2" => array (
+                   //               "multiSource" => "keep"
+                   //               ),
+                   //         // This rule merges urn:oid:attribute-OID-3 if the values are one of
+                   //         // "value1" or "value2".
+                   //         "urn:oid:attribute-OID-3" => array (
+                   //               "values" => array ("value1", "value2"),
+                   //               ),
+                   //         // This rule merges all attributes that are released by the AA
+                   //         "*" => array ()
+                   //     ),
+
+                ),
+
+
+Options
+-------
+
+The following options can be used when configuring the '''attributeaggregator''' module
+
+### entityId
+The entityID of the Attribute Authority. The metadata of the AA must be in the
+attributeauthority-remote metadata set.
+
+### required
+If set to TRUE, the module will throw an exception if the attribute query can not be
+attempted or when there is an error during the query. If FALSE, the request processing
+will resume on errors.
+
+### attributeId
+This is the *Subject* in the issued AttributeQuery. The attribute must be previously 
+resolved by an authproc module. The default attribute is urn:oid:1.3.6.1.4.1.5923.1.1.1.6 
+(eduPersonPrincipalName).
+
+### attributeNameFormat
+The format of the NameID in the issued AttributeQuery. The default value is 
+`urn:oasis:names:tc:SAML:2.0:attrname-format:uri`.
+
+### attributes
+The `attributes` element specifies what to do with the attributes received from the AA.
+The keys of the array is the attribute name in __urn:oid__ format. A special index "*"
+applies to all attributes that are not specified as an array index.
+
+If the attributes element is undefined or empty, the filter will request all attributes and all
+received attributes will be merged.
+
+For each attribute the following filters can be specified:
+
+* `values`:    the array of acceptable values. If not defined, the filter will accept all values.
+* `multiSource`:
+  * `merge`:    merge the existing and the new values, this is the default behaviour,
+  * `override`: drop the existing values and set the values from AA,
+  * `keep`:     drop the new values from AA and preserve the original values. Note that it
+    only preserves existing attributes, so if an attribute does not exist
+    before the filter is run, the values provided by the AA will be assigned.
+    Thus, if you want to limit what attributes you accept from the AA, you
+    can not use the default ('*') rule.
+
+What's new in 2.x
+-----------------
+Despite the major rewrite of the module, the configuration and the behaviour has only slightly
+changed:
+
+* the `required` option covers the whole process of the attribute query
+* new wildcard ('*') attribute definition
+* attribute values are actually filtered out after attribute resolution
---
+++ lib/Auth/Process/attributeaggregator.php
@@ -0,0 +1,391 @@
+<?php
+/**
+ * Attribute Aggregator Authentication Processing filter
+ *
+ * Filter for requesting the vo to give attributes to the SP.
+ *
+ * @author Gyula Szabó <gyufi@niif.hu>
+ * @package simpleSAMLphp
+ * @version $Id$
+ */
+
+class sspmod_attributeaggregator_Auth_Process_attributeaggregator extends SimpleSAML_Auth_ProcessingFilter
+{
+
+    /**
+     *
+     * AA IdP entityId
+     * @var string
+     */
+    private $entityId = null;
+
+    /**
+     *
+     * attributeId, the key of the user in the AA. default is eduPersonPrincipalName
+     * @var unknown_type
+     */
+    private $attributeId = "urn:oid:1.3.6.1.4.1.5923.1.1.1.6";
+
+    /**
+     *
+     * If set to TRUE, the module will throw an exception on runtime errors
+     * @var boolean
+     */
+    private $required = FALSE;
+
+    /**
+     * 
+     * nameIdFormat, the format of the attributeId. Default is "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent";
+     * @var unknown_type
+     */
+    private $nameIdFormat = SAML2\Constants::NAMEID_PERSISTENT;
+
+
+    /**
+     * Array of the requested attributes. Key is the attribute name, array structure is
+     *   * 'values' => array()
+     *   * 'multiSource' => "merge|keep|override"
+     *
+     * @var array
+     */
+    private $attributes = array();
+
+    /**
+     * nameFormat of attributes. Default is "urn:oasis:names:tc:SAML:2.0:attrname-format:uri"
+     * @var string
+     */
+    private $attributeNameFormat = "urn:oasis:names:tc:SAML:2.0:attrname-format:uri";
+
+    /**
+     * The metadata of the AA
+     *
+     * @var SimpleSAML_Configuration
+     */
+    private $aaMetadata;
+
+    /**
+     * The URL to direct the SAML2 Attribute Query to
+     *
+     * @var string
+     */
+    private $aaEndpoint='';
+
+    /**
+     * The metadata of this SP
+     *
+     * @var SimpleSAML_Configuration
+     */
+    private $selfMetadata;
+
+    /**
+     * Initialize attributeaggregator filter
+     *
+     * Validates and parses the configuration
+     *
+     * @param array $config   Configuration information
+     * @param mixed $reserved For future use
+     */
+    public function __construct($config, $reserved)
+    {
+        assert('is_array($config)');
+        parent::__construct($config, $reserved);
+
+        $metadata = SimpleSAML_Metadata_MetaDataStorageHandler::getMetadataHandler();
+
+        // XXX We can't initialize selfMetadata now, because we can't access $state here
+        // $this->selfMetadata = $metadata->getMetaDataCurrent('saml20-sp-hosted') fails
+        // so we need to take the path $state->authsourceid->authsource->metadata once
+        // we have $state in ::process()
+
+        if (!empty($config["attributeId"])){
+            $this->attributeId = $config["attributeId"];
+        }
+        
+        if (!empty($config["required"])){
+            $this->required = $config["required"];
+        }
+
+        if (!empty($config["nameIdFormat"])) {
+            $this->nameIdFormat = $config["nameIdFormat"];
+        }
+
+        if (!empty($config["attributes"])){
+            if (! is_array($config["attributes"])) {
+                throw new SimpleSAML\Error\Exception("attributeaggregator: Invalid format of attributes array in the configuration");
+            }
+            foreach ($config["attributes"] as $attribute) {
+                if (! is_array($attribute)) {
+                    throw new SimpleSAML\Error\Exception("attributeaggregator: Invalid format of attributes array in the configuration");
+                }
+                if (array_key_exists("values", $attribute)) {
+                    if (! is_array($attribute["values"])) {
+                        throw new SimpleSAML\Error\Exception("attributeaggregator: Invalid format of attributes array in the configuration");
+                    }    
+                }
+                if (array_key_exists('multiSource', $attribute)){
+                    if(! preg_match('/^(merge|keep|override)$/', $attribute['multiSource']))
+                        throw new SimpleSAML\Error\Exception(
+                            'attributeaggregator: Invalid multiSource value "'.
+                            $attribute['multiSource'].'" for '.key($attribute).
+                            '. Should be one of keep, merge or override.'
+                    );
+                }
+            }
+            $this->attributes = $config["attributes"];
+        }
+
+        if (!empty($config["attributeNameFormat"])){
+            $this->attributeNameFormat = $config["attributeNameFormat"];
+        }
+
+        if (isset($config['entityId'])) {
+            $this->entityId = $config['entityId'];
+
+            try {
+                $this->aaMetadata = SimpleSAML_Configuration::loadFromArray(
+                    $metadata->getMetaData($this->entityId, 'saml20-idp-remote')
+                );
+
+                if ($this->aaMetadata->hasValue('AttributeService')) {
+                    foreach ($this->aaMetadata->getArray('AttributeService',array()) as $aa_endpoint) {
+                        if ($aa_endpoint['Binding'] == SAML2\Constants::BINDING_SOAP) {
+                            $this->aaEndpoint = $aa_endpoint['Location'];
+                            break;
+                        }
+                    }
+                }
+                if(empty($this->aaEndpoint)) {
+                    throw new RuntimeException($this->entityId.' does not have a usable AttributeService endpoint in metadata'.
+                        ' with binding '.SAML2\Constants::BINDING_SOAP);
+                }
+            } catch (Exception $e) {
+                SimpleSAML\Logger::warning('Unable to perform attribute query, no metadata for '.$this->entityId);
+                if ($this->required) {
+                    throw $e;
+                }
+            }
+        }
+        else {
+            throw new SimpleSAML\Error\Exception(
+                'attributeaggregator: AA entityId is not specified in the configuration.'
+            );
+        }
+    }
+
+    /**
+     * Add attributes after querying attributes from an attribute authority
+     *
+     * @param array &$state The state of the response.
+     */
+    public function process(&$state)
+    {
+        assert('is_array($state)');
+
+        if (empty($this->aaMetadata) || empty($this->aaEndpoint)) {
+            // We can not do anything without AA metadata but we may let others run
+            // Should only reach here with $this->required===false
+            SimpleSAML\Logger::debug('No AA metadata, aborting attribute query');
+            return;
+        }
+
+        
+
+        // We need to initialize $this->selfMetadata here, because we have just now learnt
+        // the authsource we are using
+        if (empty($state["saml:sp:State"]["saml:sp:AuthId"])) {
+            SimpleSAML\Logger::error("Unable to access the auth source ID. Are we a SAML2 SP?");
+            throw new SimpleSAML\Error\Exception("Unable to access the auth source ID");
+        }
+        $authsource = simpleSAML_Auth_Source::getById($state["saml:sp:State"]["saml:sp:AuthId"]);
+        // This must be instanceof sspmod_saml_Auth_Source_SP
+        if (!($authsource instanceof sspmod_saml_auth_Source_SP)) {
+            // XXX The class name above might change in the future!
+            throw new SimpleSAML\ErrorException("Auth source is not a SAML SP");
+        }
+        $this->selfMetadata = $authsource->getMetadata();
+
+        try {
+            // verify that we are having all the necessary information
+            if (empty($state['Attributes'][$this->attributeId]) ||
+                count($state['Attributes'][$this->attributeId]) > 1) {
+                throw new RuntimeException("Can't attempt attribute query, attribute ".
+                    $this->attributeId." is not present or has multiple values");
+            }
+
+            //$this->mergeAttributes($state, Array( 'test'=> ['hello'] ));
+
+            // build attribute query
+            $query = new SAML2\AttributeQuery();
+            $query->setDestination($this->aaEndpoint);
+            $query->setIssuer($this->selfMetadata->getValue('entityID'));
+            //$query->setIssuer('https://testssp.kreonet.net/sp/simplesamlphp');
+            $nameid = SAML2\XML\saml\NameID::fromArray (
+                array(
+                    'Value' => $state['Attributes'][$this->attributeId][0],
+                    'Format' => $this->nameIdFormat
+                )
+            );
+    
+            $nameid->SPProvidedID = '';
+
+            $attr = $this->getRequestedAttributes();
+            //$attr['email'] = $state['Attributes']['urn:oid:0.9.2342.19200300.100.1.3'];
+            //$attr['urn:oid:1.3.6.1.4.1.5923.1.5.1.1'] = Array('test:group');
+            //SimpleSAML\Logger::info('[attrs]'.json_encode($attr));
+
+            $query->setNameId($nameid);
+            $query->setAttributeNameFormat($this->attributeNameFormat);
+            $query->setAttributes($attr); // may be empty, then it's a noop
+            $query->setID(SimpleSAML\Utils\Random::generateID());
+            // TODO: should this call be made optional?
+
+            //sspmod_saml_Message::addSign($this->selfMetadata,$this->aaMetadata,$query);
+            sspmod_saml_Message::addSign($this->selfMetadata,$this->aaMetadata,$query);
+
+            SimpleSAML\Logger::error($this->entityId);
+        //    SimpleSAML\Logger::info(strip_tags($query->toUnsignedXML()));
+
+            // send attribute query
+            SimpleSAML\Logger::debug('Sending attribute query: '.var_export($query,true));
+            $binding = new SAML2\SOAPClient();
+            
+            $response = $binding->send(
+                $query, 
+                $this->selfMetadata
+                //$this->aaMetadata
+            );
+
+            // verify result
+            SimpleSAML\Logger::debug('Received attribute response: '.var_export($response,true));
+            if (!$response->isSuccess()) {
+                throw new RuntimeException('Got a SAML error on attribute query ('.
+                    $response->getStatus()['Code'].')');
+            }
+            // merge attributes
+            $assertions = $response->getAssertions();
+
+            foreach($assertions as $assertion) {
+                //$assertion = $response->getAssertions()[0]; // TODO Can there be more than 1?
+                if (empty($assertion)) {
+                    throw new RuntimeException('Got an empty SAML Response');
+                }
+                SimpleSAML\Logger::info("[aa] ".json_encode($assertion->getAttributes()));
+                $this->mergeAttributes($state, $assertion->getAttributes());
+            }
+        } catch (Exception $e) {
+            SimpleSAML\Logger::error("Attribute query failed: ".$e->getMessage());
+            if ($this->required) {
+                throw $e;
+            }
+        }
+    }
+
+    private function mergeAttributes(&$state, $attributes_from_aa) {
+        if (empty($attributes_from_aa)) {
+            return;
+        }
+
+        foreach ($attributes_from_aa as $name => $values) {
+            // Is there a merge rule for this attribute?
+            if (array_key_exists($name, $this->attributes) ||
+                array_key_exists('*', $this->attributes)) {
+                // Filter out values that don't match
+                $this->filter_attribute_values($name,$values);
+                if (empty($values)) {
+                    SimpleSAML\Logger::info("[attributeaggregator] No values left for attribute ".$name);
+                    continue;
+                }
+
+                // Try to obtain a merge policy. It may remain undefined
+                @$mergePolicy = (isset($this->attributes[$name]) ?
+                    $this->attributes[$name]['multiSource'] :
+                    $this->attributes['*']['multiSource']);
+
+                // Are we having values for the attribute before the merge?
+                if (array_key_exists($name, $state['Attributes'])) {
+                    // Do we have a merge policy?
+                    if (isset($mergePolicy)) {
+                        switch ($mergePolicy) {
+                        case 'override':
+                            self::override_attribute_values($state,$name,$values);
+                            break;
+                        case 'keep':
+                            SimpleSAML\Logger::info('[attributeaggregator] Keeping attribute '.$name);
+                            continue 2;
+                            break;
+                        case 'merge':
+                            self::merge_attribute_values($state,$name,$values);
+                            break;
+                        }
+                    }
+                    // if no merge policy but the attribute is specified (or the config has a
+                    // '*' default element), merge the values
+                    else {
+                        self::merge_attribute_values($state,$name,$values);
+                    }
+                } else {
+                    // if the attribute hasn't existed yet, let's create it
+                    self::override_attribute_values($state,$name,$values);
+                }
+            }
+            // the attribute isn't specified in the config
+            // if we have _anything_ in the attribute config, we drop what we have just received,
+            // but if the attribute config part is empty, we merge it.
+            else {
+                if (empty($this->attributes)) {
+                    self::merge_attribute_values($state,$name,$values);
+                } else {
+                    SimpleSAML\Logger::info("[attributeaggregator] Rejecting attribute ".$name);
+                    continue;
+                }
+            }
+        }
+    }
+
+    private function filter_attribute_values($name,&$values) {
+        if (isset($this->attributes[$name]['values'])) {
+            $values = array_intersect($values,$this->attributes[$name]['values']);
+        } elseif (isset($this->attributes['*']['values'])) {
+            $values = array_intersect($values,$this->attributes['*']['values']);
+        }
+        // or do nothing
+    }
+
+    private static function merge_attribute_values (&$state,$name,$values) {
+        if (isset($state['Attributes'][$name])) {
+            SimpleSAML\Logger::info('[attributeaggregator] Merging attribute '.$name);
+            $state['Attributes'][$name] = array_merge($state['Attributes'][$name],$values);
+        } else {
+            self::override_attribute_values($state,$name,$values);
+        }
+    }
+
+    private static function override_attribute_values (&$state,$name,$values) {
+        if (!empty($state['Attributes'][$name])) {
+            SimpleSAML\Logger::info('[attributeaggregator] Overriding attribute '.$name);
+        } else {
+            SimpleSAML\Logger::info('[attributeaggregator] Getting new attribute '.$name);
+        }
+        $state['Attributes'][$name] = $values;
+    }
+
+    private function getRequestedAttributes() {
+        if (!sizeof(array_keys($this->attributes))) {
+            return array();
+        }
+        $requestedAttributes = array_flip(array_keys($this->attributes));
+        if(array_key_exists('*',$requestedAttributes)) {
+            unset($requestedAttributes['*']);
+        }
+        foreach ($requestedAttributes as $attribute) {
+            /* Reasons for NOT including the acceptable values in the request:
+             *   - the SP might not want to let the AA know which values it accepts,
+             *   - we can't rely on that the AA sends the matching values anyway,
+             *   - in the future we might support regexp in the values array.
+             */
+            $requestedAttribute[$attribute] = array();
+        }
+        return $requestedAttributes;
+    }
+
+}
